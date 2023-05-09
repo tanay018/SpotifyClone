688fc14609f5048fdaee7043f7f645d6b28adc03
Tanay Bhuta <tanaybhuta07@gmail.com>
2023-05-10 01:15:46 +0530

    Added my project

--- spotify_backend/index.js
+++ spotify_backend/index.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
 const port = 8000;
 // API : GET type : / : return text "Hello world"
+mongoose.connect("mongodb+srv://tanaybhuta:"+ process.env.MONGO_PASSWORD +"@cluster0.uui9fqt.mongodb.net/?retryWrites=true&w=majority",
+{
+    userNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then((x) => {
+    console.log("Connected to Mongo!");
+})
+.catch((err) => {
+    console.log("Error while connecting to Mongo");
+});
 app.get("/", (req, res) => {
     // req contains all data for the request
     // res contains all data for the response

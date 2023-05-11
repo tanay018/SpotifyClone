10a1ba9465d3f84d6f1b9d03f9b3a73327928e16
Tanay Bhuta <tanaybhuta07@gmail.com>
2023-05-12 01:05:05 +0530

    Added my project

--- spotify_backend/index.js
+++ spotify_backend/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const JwtStrategy = require("passport-jwt").Strategy,
+    ExtractJwt = require("passport-jwt").ExtractJwt;
+const passport = require("passport");
+const User = require("./models/User");
+const authRoutes = require("./routes/auth");
+const songRoutes = require("./routes/song");
+const playlistRoutes = require("./routes/playlist");
 require("dotenv").config();
+const cors = require("cors");
 const app = express();
-const port = 8000;
-console.log(process.env.MONGO_URI);
+const port = 8080;
+// console.log(process.env.MONGO_URI);
 // API : GET type : / : return text "Hello world"
 mongoose
   .connect(
@@ -18,6 +26,28 @@ mongoose
   .catch((err) => {
     console.log("Error while connecting to Mongo",err);
   });
+  
+// setup passport-jwt
+let opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey =  process.env.secretorkey;
+passport.use(
+    new JwtStrategy(opts, function (jwt_payload, done) {
+        User.findOne({id: jwt_payload.sub}, function (err, user) {
+            // done(error, doesTheUserExist)
+            if (err) {
+                return done(err, false);
+            }
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+                // or you could create a new account
+            }
+        });
+    })
+);
+
 app.get("/", (req, res) => {
   // req contains all data for the request
   // res contains all data for the response

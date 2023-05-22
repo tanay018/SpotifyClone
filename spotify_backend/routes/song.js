4b652cb8e49ef30d584a44d51ed6e7f796432e70
Tanay Bhuta <tanaybhuta07@gmail.com>
2023-05-23 00:25:32 +0530

    Added my project

--- spotify_backend/routes/song.js
+++ spotify_backend/routes/song.js
@@ -35,37 +35,37 @@ router.get(
 
 // Get route to get all songs any artist has published
 // I will send the artist id and I want to see all songs that artist has published.
-// router.get(
-//     "/get/artist/:artistId",
-//     passport.authenticate("jwt", {session: false}),
-//     async (req, res) => {
-//         const {artistId} = req.params;
-//         // We can check if the artist does not exist
-//         const artist = await User.findOne({_id: artistId});
-//         // ![] = false
-//         // !null = true
-//         // !undefined = true
-//         if (!artist) {
-//             return res.status(301).json({err: "Artist does not exist"});
-//         }
+router.get(
+    "/get/artist/:artistId",
+    passport.authenticate("jwt", {session: false}),
+    async (req, res) => {
+        const {artistId} = req.params;
+        // We can check if the artist does not exist
+        const artist = await User.findOne({_id: artistId});
+        // ![] = false
+        // !null = true
+        // !undefined = true
+        if (!artist) {
+            return res.status(301).json({err: "Artist does not exist"});
+        }
 
-//         const songs = await Song.find({artist: artistId});
-//         return res.status(200).json({data: songs});
-//     }
-// );
+        const songs = await Song.find({artist: artistId});
+        return res.status(200).json({data: songs});
+    }
+);
 
-// // Get route to get a single song by name
-// router.get(
-//     "/get/songname/:songName",
-//     passport.authenticate("jwt", {session: false}),
-//     async (req, res) => {
-//         const {songName} = req.params;
+// Get route to get a single song by name
+router.get(
+    "/get/songname/:songName",
+    passport.authenticate("jwt", {session: false}),
+    async (req, res) => {
+        const {songName} = req.params;
 
-//         // name:songName --> exact name matching. Vanilla, Vanila
-//         // Pattern matching instead of direct name matching.
-//         const songs = await Song.find({name: songName});
-//         return res.status(200).json({data: songs});
-//     }
-// );
+        // name:songName --> exact name matching. Vanilla, Vanila
+        // Pattern matching instead of direct name matching.
+        const songs = await Song.find({name: songName});
+        return res.status(200).json({data: songs});
+    }
+);
 
 module.exports = router;

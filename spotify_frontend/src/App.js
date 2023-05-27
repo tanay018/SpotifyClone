9aed9df15df3999b577236095779c58e870128f6
Tanay Bhuta <tanaybhuta07@gmail.com>
2023-05-28 02:30:38 +0530

    first commit

--- spotify_frontend/src/App.js
+++ spotify_frontend/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./output.css";
+import {useState} from "react";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import LoginComponent from "./routes/Login";
+import SignupComponent from "./routes/Signup";
+import HomeComponent from "./routes/Home";
+import LoggedInHomeComponent from "./routes/LoggedInHome";
+import UploadSong from "./routes/UploadSong";
+import MyMusic from "./routes/MyMusic";
+import {useCookies} from "react-cookie";
+import songContext from "./contexts/songContext";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [currentSong, setCurrentSong] = useState(null);
+    const [cookie, setCookie] = useCookies(["token"]);
+
+    return (
+        <div className="w-screen h-screen font-poppins">
+            <BrowserRouter>
+                {cookie.token ? (
+                    // logged in routes
+                    <songContext.Provider value={{currentSong, setCurrentSong}}>
+                        <Routes>
+                            <Route path="/" element={<HelloComponent />} />
+                            <Route
+                                path="/home"
+                                element={<LoggedInHomeComponent />}
+                            />
+                            <Route
+                                path="/uploadSong"
+                                element={<UploadSong />}
+                            />
+                            <Route path="/myMusic" element={<MyMusic />} />
+                            <Route path="*" element={<Navigate to="/home" />} />
+                        </Routes>
+                    </songContext.Provider>
+                ) : (
+                    // logged out routes
+                    <Routes>
+                        <Route path="/home" element={<HomeComponent />} />
+                        <Route path="/login" element={<LoginComponent />} />
+                        <Route path="/signup" element={<SignupComponent />} />
+                        <Route path="*" element={<Navigate to="/login" />} />
+                    </Routes>
+                )}
+            </BrowserRouter>
+        </div>
+    );
 }
+
+const HelloComponent = () => {
+    return <div>This is hello from component</div>;
+};
 
 export default App;
